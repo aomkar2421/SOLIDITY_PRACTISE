5eacbe0a6ea1030e09a1952012434678e03fe3a8
aomkar2421 <omkar1651@gmail.com>
2024-03-14 22:30:12 +0530

    14 March

--- 26_advMapping.sol
+++ 26_advMapping.sol
@@ -2,17 +2,22 @@
 pragma solidity >0.8.0;
 
 struct donar {
+
     string name;
     uint age;
     uint don;
+     
 }
 
 contract advMap{
+
     mapping (address => donar ) public donations;
+
 
     function setValue(string memory _name, uint _age, uint _don) public  {
         donations[msg.sender] = donar(_name, _age,donations[msg.sender].don + _don);
     }
+
 
     function deleteValue() public {
         delete donations[msg.sender];
